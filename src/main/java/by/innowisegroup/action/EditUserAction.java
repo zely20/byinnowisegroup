0a5ed48004a8f22f48abd805d2cefdbe01e9318f
zely20@mail.ru <Civik1984>
2021-07-17 10:53:10 +0300

    refactoring user

--- src/main/java/by/innowisegroup/action/EditUserAction.java
+++ src/main/java/by/innowisegroup/action/EditUserAction.java
@@ -18,58 +18,95 @@ public class EditUserAction implements Action {
     @Override
     public boolean execute(Input input, Store memStore) {
         memStore.findAll().forEach(System.out::println);
-        Integer id = input.askInt("select ID of user which you want to edit: ", memStore.findAll().size());
+        Integer id = input.askInt("select ID of user which you want to edit: ");
         User user = memStore.findById(id);
 
+        editNameField(input, user);
+        System.out.println();
+        editSurnameField(input, user);
+        System.out.println();
+        editEmailField(input, user);
+        System.out.println();
+        editPhoneField(input, user);
+        System.out.println();
+        addPhoneField(input, user);
+        System.out.println();
+        editRoleField(input, user);
+        System.out.println();
+        System.out.println(user);
+        memStore.replace(user);
+        return true;
+    }
+
+    private void editNameField(Input input, User user) {
         String name = input.askStr("Old name: " + user.getName() + ". If you want to keep the old name, press \"ENTER\"");
         if (isEdit(name)) {
             user.setName(name);
         }
+    }
 
+    private void editSurnameField(Input input, User user) {
         String surname = input.askStr("Old surname: " + user.getSurname() + ". If you want to keep the old surname, press \"ENTER\"");
         if (isEdit(surname)) {
             user.setSurname(surname);
         }
+    }
 
+    private void editEmailField(Input input, User user) {
         String checkEmail = input.askStr("If you want to keep the old email, press \"ENTER\". If you want to change email, press any key");
         if (isEdit(checkEmail)) {
             String email = input.askWithPattern("Old email: " + user.getEmail() + ". Enter email.", EMAIL_PATTERN);
             user.setEmail(email);
         }
-        //edit PHONE
-        System.out.println("You have the following saved phones: ");
-        for (int i = 0; i < user.getPhones().size(); i++) {
-            System.out.println(i + 1 + ". " + user.getPhones().get(i));
-        }
-        System.out.println();
+    }
 
-        String checkPhone = input.askStr("If you want to keep the old phone, press \"ENTER\". If you want to change phone, press any key");
-        if (isEdit(checkPhone)) {
-            int number = input.askInt("Select the number to change.", user.getPhones().size());
-            String phone = input.askWithPattern("Enter phone like 375 ** *******: ", PHONE_PATTERN);
-
-            user.getPhones().set(number - 1, phone);
-        }
-        //add PHONE
-        if (user.getPhones().size() < 3) {
-            String checkAddPhone = input.askStr("If you want add phone press any key, else press \"ENTER\"");
-            if (isEdit(checkAddPhone)) {
-                String phone = input.askWithPattern("Enter phone like 375 ** *******: ", PHONE_PATTERN);
-                user.getPhones().add(phone);
+    private void editPhoneField(Input input, User user) {
+        boolean flag;
+        do {
+            System.out.println("You have the following saved phones: ");
+            for (int i = 0; i < user.getPhones().size(); i++) {
+                System.out.println(i + 1 + ". " + user.getPhones().get(i));
             }
-        }
-        System.out.println();
-        //change ROLE
+            System.out.println();
+
+            String checkPhone = input.askStr("If you want to keep the old phone, press \"ENTER\". If you want to change phone, press any key");
+            flag = isEdit(checkPhone);
+            if (flag) {
+                int number = input.askInt("Select the number to change.", user.getPhones().size());
+                String phone = input.askWithPattern("Enter phone like 375 ** *******: ", PHONE_PATTERN);
+
+                user.getPhones().set(number - 1, phone);
+            }
+        } while (flag);
+
+    }
+
+    private void addPhoneField(Input input, User user) {
+        boolean flag = false;
+        do {
+            if (user.getPhones().size() < 3) {
+                String checkAddPhone = input.askStr("If you want add phone press any key, else press \"ENTER\"");
+                flag = isEdit(checkAddPhone);
+                if (flag) {
+                    String phone = input.askWithPattern("Enter phone like 375 ** *******: ", PHONE_PATTERN);
+                    user.getPhones().add(phone);
+                }
+            }
+        } while (flag);
+    }
+
+    private void editRoleField(Input input, User user) {
+        System.out.println("Role is");
         user.getRoles().forEach(System.out::println);
         String checkRole = input.askStr("If you want to keep the old ROLE, press \"ENTER\". If you want to change ROLE, press any key");
-        if (isEdit(checkRole)){
+        if (isEdit(checkRole)) {
             user.getRoles().clear();
             System.out.println("Please choose new ROLE");
             for (int i = 0; i <= 2; i++) {
                 System.out.println(i + 1 + ". " + Role.values()[i].name());
             }
             int indexRole = input.askInt("Choose Role First Level or Super_User: ") - 1;
-            if(Role.values()[indexRole].equals(Role.SUPER_ADMIN)){
+            if (Role.values()[indexRole].equals(Role.SUPER_ADMIN)) {
                 user.setRole(Role.SUPER_ADMIN);
             } else {
                 user.setRole(Role.values()[indexRole]);
@@ -80,10 +117,6 @@ public class EditUserAction implements Action {
                 user.setRole(Role.values()[indexRoleTwo]);
             }
         }
-        System.out.println();
-        System.out.println(user);
-        memStore.replace(user);
-        return true;
     }
 
     private boolean isEdit(String conditions) {
